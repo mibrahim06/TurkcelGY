123d6dfbee054b825d03769129d7bc3bb4b5a817
mibrahim06 <127131940+mibrahim06@users.noreply.github.com>
2023-03-26 18:10:25 +0300

    Add files via upload

---
+++ Homeworks[Graded]/1-SOLID/SingleResponsibilityPricinciple-SRP/SingleResponsibilityPricinciple-SRP/Item.cs
@@ -0,0 +1,24 @@
+namespace SingleResponsibilityPricinciple_SRP;
+
+/**
+ * This class represents the items in the game.
+ */
+public class Item
+{
+    private string Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    
+    /**
+     * @brief Constructor for the Item class
+     * @param id Id of the item
+     * @param name Name of the item
+     * @param price Price of the item
+     */
+    public Item(string id, string name, decimal price)
+    {
+        Id = id;
+        Name = name;
+        Price = price;
+    }
+}
---
+++ Homeworks[Graded]/1-SOLID/SingleResponsibilityPricinciple-SRP/SingleResponsibilityPricinciple-SRP/ItemShop.cs
@@ -0,0 +1,34 @@
+namespace SingleResponsibilityPricinciple_SRP;
+
+/**
+ * This class is responsible for purchasing items from the shop.
+ */
+public class ItemShop
+{
+    private readonly Database _database;
+    
+    public ItemShop(Database database)
+    {
+        _database = database;
+    }
+
+    /**
+     * @param playerId Id of the player
+     * @param itemId Id of the item
+     * @return true if the item was purchased, false otherwise
+     */
+    public bool PurchaseItem(int playerId, int itemId)
+    {
+        Item? item = _database.GetItem(itemId);
+        decimal playerCoins = _database.GetPlayerCoins(playerId);
+        
+        if(item != null && playerCoins >= item.Price)
+        {
+            _database.AddItemToPlayer(playerId, itemId);
+            _database.UpdatePlayerCoins(playerId, playerCoins - item.Price);
+            return true;
+        }
+
+        return false;
+    }
+}
